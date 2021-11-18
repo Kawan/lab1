302860a162e2544117d184411c360d108daf8754
Kawan <kawanse0@gmail.com>
2021-11-18 02:20:55 +0100

    added basic js

---
+++ app.js
@@ -0,0 +1,7 @@
+const hiddenKey = ["Blackjack", "Joker"];
+const secondKey = ["hide", "freak"];
+
+function playMe() {
+  alert("I warned you but you left me no choice...");
+  console.log("Got it");
+}
