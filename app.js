386a568dc3b47a87382b7ecf26f92fd64c602c93
Kawan <kawanse0@gmail.com>
2021-11-18 14:43:01 +0100

    added different texts

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-const hiddenKey = ["Blackjack", "Joker"];
-const secondKey = ["hide", "freak"];
+window.onload = main;
+const hiddenKey = ["Key", "key"];
 
-function playMe() {
-  alert("I warned you but you left me no choice...");
-  console.log("Got it");
+let input = "";
+
+let differentTexts = {
+  firstText: {
+    texten:
+      "Sometimes you need to try hard to reach your goal. <br> Finding solution can be hard sometimes.</br> He tells you that you have enter 6b 65 79. huh.",
+  },
+  secondText: {
+    textet: "Looks you found your first text, here is next one",
+  },
+};
+
+function main() {
+  playGame();
+}
+
+function playGame() {
+  let h2 = (document.getElementById("changeText").innerHTML +=
+    differentTexts.firstText.texten);
 }
