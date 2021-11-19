6be2be82f53bc0a52d83ec529a382313014decd7
Kawan <kawanse0@gmail.com>
2021-11-19 14:08:04 +0100

    added case for input

--- app.js
+++ app.js
@@ -1,23 +1,52 @@
 window.onload = main;
-const hiddenKey = ["Key", "key"];
 
-let input = "";
-
-let differentTexts = {
-  firstText: {
-    texten:
-      "Sometimes you need to try hard to reach your goal. <br> Finding solution can be hard sometimes.</br> He tells you that you have enter 6b 65 79. huh.",
-  },
-  secondText: {
-    textet: "Looks you found your first text, here is next one",
-  },
-};
+let i = 0;
+let differentTexts = [
+  "Sometimes you need to try hard to reach your goal. <br> Finding solution can be hard sometimes.</br> He tells you that you have enter 6b 65 79. huh.",
+  "Looks you found your first text, here is next one",
+  "congrats, you just won!",
+];
 
 function main() {
   playGame();
+  userInput();
 }
 
 function playGame() {
-  let h2 = (document.getElementById("changeText").innerHTML +=
-    differentTexts.firstText.texten);
+  let h1 = (document.getElementById("changeText").innerHTML +=
+    differentTexts[i]);
+}
+
+function clearText() {
+  let text = document.getElementById("changeText");
+  while (text.firstChild) text.removeChild(text.firstChild);
+}
+
+function clearInput() {
+  document.getElementById("user-input").value = "";
+}
+
+let validation = 0;
+
+function userInput() {
+  let input = document.getElementById("user-input").value.toLowerCase();
+  switch (input) {
+    case "key":
+      if (validation == 0) {
+        clearText();
+        clearInput();
+        i += 1;
+        validation += 1;
+        playGame();
+      }
+      break;
+    case "something":
+      if (validation == 1) {
+        clearText();
+        clearInput();
+        i += 1;
+        playGame();
+      }
+      break;
+  }
 }
