911642622b894742dcd898cfcd8468363c4cd952
Kawan <kawanse0@gmail.com>
2021-11-19 16:34:19 +0100

    moved files to static and added comments

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## Lab 1
+
+Here is simple script for my homework made in JS.
+
+## Demo:
+
+[link](http://kawandev.co/lab1/)
--- static/app.js
+++ static/app.js
@@ -1,33 +1,41 @@
+// Wait for the website to load
 window.onload = main;
 
+// Where it picks up first sentence
 let i = 0;
+
+// Different texts where we can use it later
 let differentTexts = [
   "Sometimes you need to try hard to reach your goal. <br> Finding solution can be hard sometimes.</br> He tells you that you have enter 6b 65 79. huh.",
-  "Looks you found your first text, here is next one",
-  "congrats, you just won!",
+  "Looks you found your first text, here is next one. Don't use inspect element.",
+  "congrats, you just found every word",
 ];
 
+// Call the function to load
 function main() {
   playGame();
   userInput();
 }
 
+// Get the text and put the first sentence
 function playGame() {
-  let h1 = (document.getElementById("changeText").innerHTML +=
-    differentTexts[i]);
+  document.getElementById("changeText").innerHTML += differentTexts[i];
 }
 
+// Clear text in every corrected answer
 function clearText() {
   let text = document.getElementById("changeText");
   while (text.firstChild) text.removeChild(text.firstChild);
 }
 
+// Clear input text in every corrected answer
 function clearInput() {
   document.getElementById("user-input").value = "";
 }
 
 let validation = 0;
 
+// User input where it checks if the case same as inputed and starts the next question
 function userInput() {
   let input = document.getElementById("user-input").value.toLowerCase();
   switch (input) {
@@ -40,7 +48,7 @@ function userInput() {
         playGame();
       }
       break;
-    case "something":
+    case "element":
       if (validation == 1) {
         clearText();
         clearInput();
